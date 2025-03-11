c340b20f10d04e36c795fd257b9276524ca0ef15
Pedro Barreto de Souza <pedro.prd225@gmail.com>
2025-03-11 10:52:18 -0400

    v2

--- end_node/original_end_node/original_end_node.ino
+++ end_node/original_end_node/original_end_node.ino
@@ -8,6 +8,32 @@
 //End Imports/
 
 //Global Variables
+#define TAMANHO_MAXIMO 5
+#define EEPROM_SIZE 512
+#define S_To_uS_Factor 1000000ULL
+#define Time_To_Sleep 60
+
+struct Limite {
+    const char* categoria;
+    float limite_inferior;
+    float limite_superior;
+};
+
+Limite outliers[] = {
+    {"Temperatura", 0, 100},
+    {"PH", 3, 9}
+};
+
+
+// Variáveis armazenadas na memória RTC
+RTC_DATA_ATTR float valores_temperatura[TAMANHO_MAXIMO] = {0};
+RTC_DATA_ATTR float valores_ph[TAMANHO_MAXIMO] = {0};
+RTC_DATA_ATTR int num_valores_temperatura = 0;
+RTC_DATA_ATTR int num_valores_ph = 0;
+
+//PH Variables
+float valor_calibracao = 21.5; // Fator de calibração para o ajuste fino
+//End PH Variables
 
 //Lora Variables
 
@@ -25,8 +51,8 @@ const int SF = 12; //Spreading Factor : {7, 8, 9, 10, 11, 12}
 const int CR = 8; //Coding Rate 4/: {5, 6, 7, 8}
 const int BW = 125000; //BandWidth:{125000, 500000}
 
-//Demais variáveis
-byte msgCount = 0;            // Contador do número de mensagens, utilizado para distriuição de ID da mensagem
+// //Demais variáveis
+RTC_DATA_ATTR byte msgCount = 0;            // Contador do número de mensagens, utilizado para distriuição de ID da mensagem
 bool ACK = false;             //Utilizado para repetir o envio até receber o ACK
 byte localAddress = 0x1E;     //Endereço do dispositivo, 20
 byte destination = 0xFF;      //Endereço de Destino, FF = Broadcast (255)
@@ -46,13 +72,13 @@ THERMISTOR thermistor(NTC_PIN,        // Analog pin
 float temp; //Variável para armazenar o valor da temperatura
 
 //End Thermistor Variables
+
+
 // ADS Variables
 Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 //End ADS Variables
 
-//PH Variables
-float valor_calibracao = 21.5; // Fator de calibração para o ajuste fino
-//End PH Variables
+
 //TDS Variables
 #define TdsSensorPin 36 //Pino onde está o TDS
 GravityTDS gravityTds;  //Variável utilizada pela biblioteca para realizar as medições
@@ -64,6 +90,8 @@ void setup() {
   delay(1000);
   while (!Serial);
   //End Serial Setup
+  esp_sleep_enable_timer_wakeup(Time_To_Sleep * S_To_uS_Factor);
+  //
 
   //LoRa Setup
   SPI.begin(SCK, MISO, MOSI, SS); //SPI LoRa pins
@@ -111,14 +139,14 @@ void loop() {
   Serial.print("Dados dos sensores: ");
   Serial.println(outgoing);            // outgoing message
   // while(!ACK){
-    sendMessage(outgoing);  //Inicializa o pacote que vai ser enviado atráves de LoRa
+  sendMessage(outgoing);  //Inicializa o pacote que vai ser enviado atráves de LoRa
    // onReceive(LoRa.parsePacket());
     // delay(10000);
   // }
   msgCount++;
   // ACK = false;
   // delay(300000);
-  delay(60000);
+  esp_deep_sleep_start();
 }
 
 void sendMessage(String msg){
@@ -128,8 +156,8 @@ void sendMessage(String msg){
   Serial.println(msg);
 
   LoRa.beginPacket();               // start packet
-  LoRa.write(destination);         // add destination address
-  LoRa.write(localAddress);        // add sender address
+  // LoRa.write(destination);         // add destination address
+  // LoRa.write(localAddress);        // add sender address
   LoRa.write(msgCount);            // add message ID
   LoRa.write(msg.length());        // add payload length
   LoRa.print(msg);                 // add payload
@@ -138,9 +166,9 @@ void sendMessage(String msg){
 
 String coleta(){
   String msg;
-  msg += coleta_temperatura();
+  msg += String(coleta_temp()) + "ºC;";
   msg += coleta_PH();
-  msg += coleta_TDS();
+  msg += "0.0ppm;";
   msg += "0.0ntu";
   return msg;
 }
@@ -157,6 +185,7 @@ String coleta_temperatura(){
 
 String coleta_PH(){
   float valor_pH = -5.70 * ads.computeVolts(ads.readADC_SingleEnded(0)) + valor_calibracao; //Calcula valor de pH
+  valor_pH = aplicar_filtro(valor_pH, "pH");
   return "pH "+ String(valor_pH) + ";";
 }
 
@@ -172,7 +201,12 @@ String coleta_TDS(){
 float coleta_temp() {
     int leitura = thermistor.read();
     float temp = leitura / 100.0; // Ajuste da leitura
+    temp = aplicar_filtro(temp, "Temperatura");
     return temp;
+}
+
+float coleta_ph(){
+  return -5.70 * ads.computeVolts(ads.readADC_SingleEnded(0)) + valor_calibracao;
 }
 
 // String coleta_NTU(){
@@ -222,4 +256,63 @@ void onReceive(int packetSize) {
   Serial.println();
   ACK = true;
   return;
+}
+
+
+// Função para obter os limites da categoria
+Limite* obter_limites(const char* categoria) {
+    for (int i = 0; i < sizeof(outliers) / sizeof(outliers[0]); i++) {
+        if (strcmp(outliers[i].categoria, categoria) == 0) {
+            return &outliers[i];
+        }
+    }
+    return NULL;
+}
+
+// Função para calcular a média dos valores
+float calcular_media(float* valores, int num_valores) {
+    if (num_valores < 2) return valores[num_valores - 1];
+    float soma = 0;
+    for (int i = 0; i < num_valores - 1; i++) {
+        soma += valores[i];
+    }
+    return round(soma / (num_valores - 1) * 100) / 100.0;
+}
+
+// Função para calcular o desvio padrão
+float calcular_desvio_padrao(float* valores, int num_valores) {
+    if (num_valores < 2) return 0;
+    float media = calcular_media(valores, num_valores);
+    float soma = 0;
+    for (int i = 0; i < num_valores - 1; i++) {
+        soma += pow(valores[i] - media, 2);
+    }
+    return sqrt(soma / (num_valores - 1));
+}
+
+// Função para aplicar o filtro e adicionar o valor
+float aplicar_filtro(float novo_valor, const char* categoria) {
+    Limite* limites = obter_limites(categoria);
+    if (!limites) return 0;
+
+    float* valores = strcmp(categoria, "Temperatura") == 0 ? valores_temperatura : valores_ph;
+    int* num_valores = strcmp(categoria, "Temperatura") == 0 ? &num_valores_temperatura : &num_valores_ph;
+
+    if (*num_valores >= TAMANHO_MAXIMO) {
+        for (int i = 0; i < TAMANHO_MAXIMO - 1; i++) {
+            valores[i] = valores[i + 1];
+        }
+        (*num_valores)--;
+    }
+
+    valores[(*num_valores)++] = novo_valor;
+    if (novo_valor < limites->limite_inferior || novo_valor > limites->limite_superior) {
+        return valores[*num_valores - 1] = calcular_media(valores, *num_valores);
+    }
+    float media = calcular_media(valores, *num_valores);
+    float desvio_padrao = calcular_desvio_padrao(valores, *num_valores);
+    if (desvio_padrao > 0 && fabs(novo_valor - media) > 3 * desvio_padrao) {
+        return valores[*num_valores - 1] = calcular_media(valores, *num_valores);
+    }
+    return round(novo_valor * 100) / 100.0;
 }
